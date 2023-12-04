201f4c5545086a29a1b9961069852b6337e8f7b5
gu-rahl <138145056+gu-rahl@users.noreply.github.com>
2023-12-04 19:21:04 +0300

    greeting done

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
+}
